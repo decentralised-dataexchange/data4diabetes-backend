bd2920a0eed2b4f85e269e71825637556cadd3c3
George J Padayatti <george.padayatti@igrant.io>
2023-04-17 09:03:35 +0530

    Add: Write test cases for user model
    Signed-off-by: George J Padayatti <george.padayatti@igrant.io>

--- user/tests.py
+++ user/tests.py
@@ -1,3 +1,33 @@
 from django.test import TestCase
+from .models import User
 
-# Create your tests here.
+
+class UserTestCase(TestCase):
+    def setUp(self):
+        user = User(username="user001", 
+                    mobile_number="+91000000", 
+                    firstname="John", 
+                    lastname="Doe")
+        user.set_unusable_password()
+        user.save()
+
+
+    def test_user_created_with_mobile_number(self):
+        """Test user has mobile number"""
+        user = User.objects.get(username="user001")
+        self.assertEqual(user.mobile_number, "+91000000")
+    
+    def test_user_created_with_firstname(self):
+        """Test user has firstname"""
+        user = User.objects.get(username="user001")
+        self.assertEqual(user.firstname, "John")
+    
+    def test_user_created_with_lastname(self):
+        """Test user has lastname"""
+        user = User.objects.get(username="user001")
+        self.assertEqual(user.lastname, "Doe")
+    
+    def test_user_created_with_no_password(self):
+        """Test user with no password"""
+        user = User.objects.get(username="user001")
+        self.assertEqual(user.has_usable_password(), False)
